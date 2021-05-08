1ea6d1d9270425f2e14f064ae1eec41fdb6d45c4
Nikolai Kozak <nikokozak@gmail.com>
2021-05-08 11:50:18 -0400

    scons, build dir

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, const char * argv[])
 {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+    printf("compiled correctly\n");
     return 0;
 }
