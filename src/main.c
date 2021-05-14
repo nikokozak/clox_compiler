e7a56193a8c2444be8d367e7fb768b72061ddf65
Nikolai Kozak <nikokozak@gmail.com>
2021-05-14 12:25:16 -0400

    start implementing vm

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char * argv[])
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -11,12 +14,12 @@ int main(int argc, const char * argv[])
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
     writeChunk(&chunk, OP_RETURN, 124);
     disassembleChunk(&chunk, "test chunk");
 
+    interpret(&chunk);
+
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
